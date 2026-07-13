65fc69615df99e413090b90d53436f0c86ceed35
chaturyasworld <chaturyasworld0608@gmail.com>
2026-07-13 20:45:12 +0530

    a

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@
       fontSize: "1rem",
     }}
   >
-    Explore some beautiful gifts and fancy collections available at Chaturya&apos;s World.
+    Explore some beautiful gifts and fancy collections available at Chaturya World.
   </p>
 
   <div
